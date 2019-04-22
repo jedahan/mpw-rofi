dc1f90fd8f003081a98bc92b2738fd07a0c3225e
Jakob Kukla <jakob.kukla@gmail.com>
2019-04-22 22:14:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mpw-rofi
+A bash script for [MasterPassword](https://gitlab.com/MasterPassword/MasterPassword) integration in [rofi](https://github.com/davatorium/rofi). This was originally intended for private use.
+## Getting Started
+### Prerequisites
+- `rofi`
+- `masterpassword-cli`
+- `jq`
+- `xclip` or `wl-clipboard`
+### Usage
+`$ bash mpw-rofi.sh`
+### To do
+- Add feedback for wrong password
+### License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ mpw-rofi.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+mpw() {
+    _copy() {
+	if hash pbcopy 2>/dev/null; then
+            pbcopy
+        elif hash xclip 2>/dev/null; then
+            xclip -selection clip
+	elif hash wl-copy 2>/dev/null; then 
+            wl-copy
+        else
+            cat; echo 2>/dev/null
+            return
+	fi
+        echo >&2 "Copied!"
+    }
+
+    # Empty the clipboard
+    :| _copy 2>/dev/null
+
+    # Start Master Password and copy the output.
+    printf %s "$(MPW_FULLNAME=$MPW_FULLNAME command mpw "$@")" | _copy
+}
+
+count=$(ls -1 $HOME/.mpw.d/*.mpsites.json 2> /dev/null | wc -l)
+if [ $count != 0 ]
+then
+    pathtoconfig=$(ls -1 $HOME/.mpw.d/*.mpsites.json 2> /dev/null | head -1)
+    fullname=${pathtoconfig%.*}
+    fullname=${fullname%.*}
+    fullname=${fullname##*/}
+else
+    fullname=$(rofi -dmenu -p "Full name")
+fi
+
+storedsites=$(cat "$HOME/.mpw.d/$fullname.mpsites.json" | jq -r '.sites | keys[]' | sort -n)
+site=$(echo -e "$storedsites" | rofi -dmenu -p "Site name")
+
+if [ -z $site ]
+then
+    exit
+fi
+echo "#/bin/bash
+rofi -dmenu -password -p 'Password'" > /tmp/mpw_askpass.sh
+chmod a+x /tmp/mpw_askpass.sh
+MPW_ASKPASS="/tmp/mpw_askpass.sh" mpw -u "$fullname" -t x "$site"
